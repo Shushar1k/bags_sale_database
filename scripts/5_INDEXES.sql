182b8dcec7f562e948ab6bb2ed1c297019834943
Shushar1k <145340194+Shushar1k@users.noreply.github.com>
2025-09-30 21:17:50 +0300

    Update 5_INDEXES.sql

--- scripts/5_INDEXES.sql
+++ scripts/5_INDEXES.sql
@@ -1,5 +1,5 @@
 /*Индекс, чтобы ускорить поиск нужной сумки, т.к. id сложно будет запомнить с расширением ассортимента, а название и цвет -- проще, поэтому далее такие поиски будет оправданы...*/
 CREATE INDEX index_model_and_color ON bags.Models(name_of_model, color);
 
-/*Этот индекс будет ускорять поиск дополнительных услуг в технической таблице.*/
-CREATE INDEX index_delivery_features ON bags.Delivery_company_features(delivery_id, feature_id);
+/*Этот индекс будет ускорять поиск покупателя по ФИО.*/
+CREATE INDEX index_name ON bags.Customers(first_name, second_name);
